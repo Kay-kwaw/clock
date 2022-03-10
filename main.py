5d5ea7447d46ef738c0919aae5ae653e8b17e69c
Kwaw Kumi <kumikwaw9@gmail.com>
2022-03-10 12:42:35 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root=Tk()
+root.title("clock")
+
+def time():
+    string = strftime('%H: %M: %S %p')
+    Label.config(text=string)
+    Label.after(1000, time)
+
+Label=Label(root, font=("ds-digital", 80), background = "black", foreground="cyan")
+Label.pack(anchor='center')
+time()
+
+
+mainloop()
